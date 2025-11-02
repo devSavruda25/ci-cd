91025b6c613db694672ef592ccae131baa31f075
devSavruda25 <developer.savruda@gmail.com>
2025-11-02 14:26:01 +0530

    code change

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-// // server.js
-// const express = require("express");
+// server.js
+const express = require("express");
 
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// // Middleware to parse JSON
-// app.use(express.json());
-
-// // Basic route
-// app.get("/", (req, res) => {
-//   res.send("Hello from Express 🚀");
-// });
-
-// // Start server
-// app.listen(PORT, () => {
-//   console.log(`✅ Server running at http://localhost:${PORT}`);
-// });
-// app.js
-const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Custom middleware
-app.use((req, res, next) => {
-  console.log(`${req.method}`);
-  console.log(`${req.url}`);
-  next(); // move to the next middleware or route handler
-});
+// Middleware to parse JSON
+app.use(express.json());
 
-// Sample route
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+// Basic route
+app.get("/", (req, res) => {
+  res.send("Hello from Express 🚀");
 });
 
 // Start server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
 });
+// app.js
+// const express = require('express');
+// const app = express();
+
+// // Custom middleware
+// app.use((req, res, next) => {
+//   console.log(`${req.method}`);
+//   console.log(`${req.url}`);
+//   next(); // move to the next middleware or route handler
+// });
+
+// // Sample route
+// app.get('/', (req, res) => {
+//   res.send('Hello from Express!');
+// });
+
+// // Start server
+// app.listen(3000, () => {
+//   console.log('Server is running on port 3000');
+// });
